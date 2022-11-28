23ac845e2cbec8cdda8fc25486bdfc791dd74785
Tricky Licky <wangombevictor5@gmail.com>
2022-11-28 08:27:37 +0300

    complete assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+    validate :must_be_clickbaity
+
+    def must_be_clickbaity
+        if !(["Won't Believe","Secret","Top [number]","Guess"].any?{|word|"#{title}".include?(word)})
+            errors.add(:title, "is not clickbait")
+        end
+      end
+
 end
